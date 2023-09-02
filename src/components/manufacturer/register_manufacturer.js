a60bebeb67c0de05ecb63eed3f2d7f4560c8b95b
gafaradetunji <gafaradetunji471@gmail.com>
2023-09-02 23:45:13 +0100

    Register auth implemented

--- src/components/manufacturer/register_manufacturer.js
+++ src/components/manufacturer/register_manufacturer.js
@@ -1,6 +1,5 @@
 import RegisterNav from "../registerNav"
 import { useNavigate, Link } from 'react-router-dom'
-import Select from 'react-select'
 import { useState } from "react"
 import RegisterMobileNav from "../registerMobileNav"
 import axios from "axios"
@@ -9,18 +8,15 @@ import axios from "axios"
 const SignUpURL = 'https://digi.mjobi.com/manufacturer/signup/'
 const RegisterManufacturer = () => {
     const navigate = useNavigate()
-    const options = [
-      { value: 'Nigeria', label: 'Nigeria' },
-      { value: 'Usa', label: 'Usa' },
-      { value: 'United Kingdom', label: 'United Kingdom' }
-    ]
 
     const [ values, setValues ] = useState({
-      location: '',
       email: '',
-      number: '',
+      owner_name: '',
+      manufacturer_name: '',
+      mobile_number: '',
       password: '',
-      confirmPassword: '',
+      nid_number: '',
+      shop_logo: '',
     })
     const [ formError, setFormError ] = useState({})
   
@@ -34,23 +30,26 @@ const RegisterManufacturer = () => {
       const emailValidation = /^[a-z0-9]+@(?:[a-z0-9]+\.)+[a-z]+$/
       const numberValidation = /^[0-9]+/
   
-      if (values.location === '') {
-        error.location = 'Location is required!'
-      }
       if (values.email === '') {
         error.email = 'Email is required!'
       }
       else if( !emailValidation.test(values.email)){
         error.email = 'Email is invalid!'
       }
-      if (values.number === '') {
-        error.number = 'Number is required!'
+      if (values.owner_name === '') {
+        error.owner_name = 'Owner"s name is required!'
       }
-      else if( !numberValidation.test(values.number)){
-        error.number = 'Expecting Numbers only'
+      if (values.manufacturer_name === '') {
+        error.manufacturer_name_name = 'Owner"s name is required!'
       }
-      else if(values.number.length !== 11){
-        error.number = 'Must be 11 Digits here'
+      if (values.mobile_number === '') {
+        error.mobile_number = 'Number is required!'
+      }
+      else if( !numberValidation.test(values.mobile_number)){
+        error.mobile_number = 'Expecting Numbers only'
+      }
+      else if(values.mobile_number.length !== 11){
+        error.mobile_number = 'Must be 11 Digits here'
       }
       const isWhitespace = /^(?=.*\s)/;
       if (isWhitespace.test(values.password)) {
@@ -72,38 +71,59 @@ const RegisterManufacturer = () => {
       if (!isContainsSymbol.test(values.password)) {
         error.password = "Password must contain at least one Special Symbol.";
       }
-      if (values.password !== values.confirmPassword) {
-        error.confirmPassword = 'Password does not match'
+      if (values.nid_number) {
+        error.nid_number = 'NID number is required'
       }
       setFormError({...error})
   
       return Object.keys(error).length < 1
     }
+
+    const handleImageUpload = (e) => {
+      const file = e.target.files[0];
+      setValues((prevValues) => ({
+        ...prevValues,
+        shop_logo: file,
+      }));
+    };
   
     const handleSubmit = async (e) => {
-      e.preventDefault()
-      console.log('Values', values)
-      const isvalid = validateForm()
-      try{
-        const response = await axios.post(SignUpURL, 
-        {
+      e.preventDefault();
+      if (values.email === '' || values.password === '') {
+        return validateForm();
+      }
+    
+      const isvalid = validateForm();
+    
+      try {
+        const response = await axios.post(SignUpURL, {
           email: values.email,
-          password: values.password 
-        })
+          owner_name: values.owner_name,
+          manufacturer_name: values.manufacturer_name,
+          mobile_number: values.mobile_number,
+          password: values.password,
+          nid_number: values.nid_number,
+          shop_logo: values.shop_logo,
+        }, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+    
         if (response.status === 202) {
           console.log('Login successful');
           console.log('Message:', response.data.message);
           console.log('Token:', response.data.token);
-  
-          isvalid && navigate('/')
-          console.log(isvalid)
+    
+          isvalid && navigate('/');
+          console.log(isvalid);
         } else {
           console.error('Login Failed:', response.status);
         }
       } catch (error) {
         console.error('Login Failed:', error);
       }
-    }
+    }    
   
     return(
         <main className='h-100'>
@@ -126,19 +146,11 @@ const RegisterManufacturer = () => {
                   <p className="">Additional Details</p>
                 </div>
               </div>
-              <label className="block py-2 w-[90%]">
-                    <span className="block text-sm ml-3 my-2 font-semibold text-slate-700">What is your location</span>
-                    <p className='error'>{formError.location}</p>
-                    <Select options={options}
-                      name="location"
-                      placeholder='Choose your location'
-                      value={values.location}
-                      onChange={handleChange}
-                      className="w-full text-[12px] text-[#2D2B4A] ml-3 rounded-md picker"/>
-              </label>
+
               <label className="block py-2 w-[90%]">
                 <span className="block text-sm ml-3 my-2 font-semibold text-slate-700">Email Address</span>
-                <input type="email" placeholder="Legal Entity" 
+                <p className='error text-[14px] text-[#df0000]'>{formError.email}</p>
+                <input type="email" placeholder="Email Address" 
                   id='email' name='email'
                   value={values.email}
                   onChange={handleChange}
@@ -146,19 +158,47 @@ const RegisterManufacturer = () => {
                   border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
                 "/>
               </label>
+
+              <label className="block py-2 w-[90%]">
+                <span className="block text-sm ml-3 my-2 font-semibold text-slate-700">What is the owner's name</span>
+                <p className='error text-[14px] text-[#df0000]'>{formError.owner_name}</p>
+                <input type="text"
+                  id="owner_name"
+                  name="owner_name"
+                  placeholder='owner name'
+                  value={values.owner_name}
+                  onChange={handleChange}
+                  className="mt-1 block w-full px-3 ml-3 py-2 bg-white 
+                  border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400"/>
+              </label>
+
+              <label className="block py-2 w-[90%]">
+                <span className="block text-sm ml-3 my-2 font-semibold text-slate-700">What is the manufacturer_name's name</span>
+                <input type="text"
+                  id="manufacturer_name"
+                  name="manufacturer_name"
+                  placeholder='manufacturer name'
+                  value={values.manufacturer_name}
+                  onChange={handleChange}
+                  className="mt-1 block w-full px-3 ml-3 py-2 bg-white 
+                  border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400"/>
+              </label>
+
               <label className="block py-2 w-[90%]">
                 <span className="block text-sm ml-3 my-2 font-semibold text-slate-700">Phone number</span>
-                <input type="number" placeholder="Legal Entity" 
-                  id='number' name='number'
-                  value={values.number}
+                <p className='error text-[14px] text-[#df0000]'>{formError.mobile_number}</p>
+                <input type="number" placeholder="Phone Number" 
+                  id='mobile_number' name='mobile_number'
+                  value={values.mobile_number}
                   onChange={handleChange}
                   className="mt-1 block w-full px-3 ml-3 py-2 bg-white 
                   border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
                 "/>
               </label>
               <label className="block py-2 w-[90%]">
-                <span className="block text-sm ml-3 my-2 font-semibold text-slate-700">Create a passowrd</span>
-                <input type="password" placeholder="Legal Entity" 
+                <span className="block text-sm ml-3 my-2 font-semibold text-slate-700">Passowrd</span>
+                <p className='error text-[14px] text-[#df0000]'>{formError.password}</p>
+                <input type="password" placeholder="Password" 
                   id='password' name='password'
                   value={values.password}
                   onChange={handleChange}
@@ -167,14 +207,25 @@ const RegisterManufacturer = () => {
                 "/>
               </label>
               <label className="block py-2 w-[90%]">
-                <span className="block text-sm ml-3 my-2 font-semibold text-slate-700">Repeat passowrd</span>
-                <input type="password" placeholder="Legal Entity" 
-                  id='confirmpassword' name='confirmpassword'
-                  value={values.confirmPassword}
+                <span className="block text-sm ml-3 my-2 font-semibold text-slate-700">NID Number</span>
+                <input type="number" placeholder="NID Number" 
+                  id='nid_number' name='nid_number'
+                  value={values.nid_number}
                   onChange={handleChange}
                   className="mt-1 block w-full px-3 ml-3 py-2 bg-white 
                   border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
                 "/>
+              </label>
+              <label className="block py-2 w-[90%]">
+                <span className="block text-sm ml-3 my-2 font-semibold text-slate-700">Shop Logo</span>
+                <input
+                  type="file"
+                  id="shop_logo"
+                  name="shop_logo"
+                  accept="image/*"
+                  onChange={(e) => handleImageUpload(e)}
+                  className="mt-1 block w-full px-3 ml-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400"
+                />
               </label>
               <button className='rounded-md mt-4 join'>Sign Up</button>
               <p className="mt-4 text-center">Don't have an account? <Link to="/" className="text-[#df0000]">Login</Link></p>
